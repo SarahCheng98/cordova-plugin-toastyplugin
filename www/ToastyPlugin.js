77542eeb4e234cadacad743df1c255162f3a2362
SarahCheng98 <chengloktung@gmail.com>
2022-07-22 10:37:54 +0800

    Added my project

---
+++ www/ToastyPlugin.js
@@ -0,0 +1,23 @@
+function ToastyPlugin() {}
+
+// The function that passes work along to native shells
+// Message is a string, duration may be 'long' or 'short'
+ToastyPlugin.prototype.show = function(message, duration, successCallback, errorCallback) {
+  console.log("Calling js.");
+  console.log("message = " + message);
+  console.log("duration = " + duration);
+  var options = {};
+  options.message = message;
+  options.duration = duration;
+  cordova.exec(successCallback, errorCallback, 'toastyplugin', 'show', [options]);
+}
+
+// Installation constructor that binds ToastyPlugin to window
+ToastyPlugin.install = function() {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+  window.plugins.toastyPlugin = new ToastyPlugin();
+  return window.plugins.toastyPlugin;
+};
+cordova.addConstructor(ToastyPlugin.install);
