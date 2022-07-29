8f55fb282a16d7a85ae7a025d0361e22622ddc34
SarahCheng98 <chengloktung@gmail.com>
2022-07-29 16:52:17 +0800

    recovered

--- src/ios/ToastyPlugin.swift
+++ src/ios/ToastyPlugin.swift
@@ -15,26 +15,28 @@ import UIKit
             seconds = 2.0
         }
 
-        let toastLabel = UILabel(frame: CGRect(x: self.viewController.view.frame.size.width/2 - 75, y: self.viewController.view.frame.size.height-100, width: 150, height: 35))
-        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
-        toastLabel.textColor = UIColor.white
-        toastLabel.textAlignment = .center;
-        toastLabel.font = .systemFont(ofSize: 12.0);
-        toastLabel.text = message
-        toastLabel.alpha = 1.0
-        toastLabel.layer.cornerRadius = 10;
-        toastLabel.clipsToBounds = true
-        self.viewController.view.addSubview(toastLabel)
-        UIView.animate(withDuration: seconds, delay: 0.1, options: .curveEaseOut, animations: {
-            toastLabel.alpha = 0.0
-        }, completion: {(isCompleted) in
-            toastLabel.removeFromSuperview()
-        })
+        if message.count > 0{
+            let toastLabel = UILabel(frame: CGRect(x: self.viewController.view.frame.size.width/2 - 75, y: self.viewController.view.frame.size.height-100, width: 150, height: 35))
+            toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+            toastLabel.textColor = UIColor.white
+            toastLabel.textAlignment = .center;
+            toastLabel.font = .systemFont(ofSize: 12.0);
+            toastLabel.text = message
+            toastLabel.alpha = 1.0
+            toastLabel.layer.cornerRadius = 10;
+            toastLabel.clipsToBounds = true
+            self.viewController.view.addSubview(toastLabel)
+            UIView.animate(withDuration: seconds, delay: 0.1, options: .curveEaseOut, animations: {
+                toastLabel.alpha = 0.0
+            }, completion: {(isCompleted) in
+                toastLabel.removeFromSuperview()
+            })
 
-        pluginResult = CDVPluginResult(
-            status: CDVCommandStatus_OK,
-            messageAs: message
-        )
+            pluginResult = CDVPluginResult(
+                status: CDVCommandStatus_OK,
+                messageAs: message
+            )
+        }
 
         self.commandDelegate!.send(
             pluginResult,
